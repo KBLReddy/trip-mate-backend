54c2090b6963c32f61a8f276e7fedc579fb9d529
Lakshman Karri <lakshmankarri24@gmail.com>
2025-07-23 13:13:41 +0530

    changed test code coverage values

--- jest.config.js
+++ jest.config.js
@@ -10,7 +10,7 @@ module.exports = {
   globalTeardown: '<rootDir>/tests/setup/global-teardown.ts',
   coverageThreshold: {
     global: {
-      branches: 75,
+      branches: 70,
       functions: 80,
       lines: 80,
       statements: 80
